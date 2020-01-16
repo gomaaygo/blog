bbb4835a4a66300362d636ea02afe3f497bd215b
MSG <maiurygarcia@gmail.com>
2020-01-16 18:38:43 -0300

    Integrando a interface

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ def post_detail(request, pk):
 
 def post_new(request):
 	if request.method == "POST":
-		form = PostForm(request.POST)
+		form = PostForm(request.POST, request.FILES)
 
 		if form.is_valid():
 			post = form.save(commit=False)
@@ -30,10 +30,11 @@ def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 
 	if request.method == "POST":
-		form = PostForm(request.POST, instance=post)
+		form = PostForm(request.POST, request.FILES, instance=post)
 
 		if form.is_valid():
 			post = form.save(commit=False)
+
 			post.author = request.user
 			post.save()
 
