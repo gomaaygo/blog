0d45d4b57c9978ce059d1b84eeec50563b5f22fc
MSG <maiurygarcia@gmail.com>
2020-01-16 17:04:09 -0300

    Adicionando método publicar

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 
 
 def post_list(request):
-	posts = Post.objects.filter(publish_date__lte=timezone.now())
+	posts = Post.objects.all()
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -19,7 +19,6 @@ def post_new(request):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.publish_date = timezone.now()
 			post.save()
 
 			return redirect('post_detail', pk=post.pk)
@@ -36,10 +35,17 @@ def post_edit(request, pk):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.publish_date = timezone.now()
 			post.save()
 
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)	
 	return render(request, 'blog/post_edit.html', {'form' : form})
+
+def publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+
+	post.publish()
+
+	return redirect('post_detail', pk=post.pk)
+
